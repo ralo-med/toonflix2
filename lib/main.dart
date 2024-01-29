70386c2e4f3c071afb9561b375e8eef98e95d4cd
Ralo <74175604+ralo-med@users.noreply.github.com>
2024-01-29 21:54:00 +0900

    #6.5 waitForWebToons

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:toonflix2/screens/home_screen.dart';
-import 'package:toonflix2/services/api_service.dart';
 
 void main() {
-  ApiService().getTodaysToons();
   runApp(const App());
 }
 
