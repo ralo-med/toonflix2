c657677588a6bc217897f07f7c321dd4e8ff8da7
Ralo <74175604+ralo-med@users.noreply.github.com>
2024-06-09 12:49:48 +0900

    #6.6 FutureBuilder

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -2,47 +2,35 @@ import 'package:flutter/material.dart';
 import 'package:toonflix2/models/webtoon_model.dart';
 import 'package:toonflix2/services/api_service.dart';
 
-class HomeScreen extends StatefulWidget {
-  const HomeScreen({super.key});
+class HomeScreen extends StatelessWidget {
+  HomeScreen({super.key});
 
-  @override
-  State<HomeScreen> createState() => _HomeScreenState();
-}
-
-class _HomeScreenState extends State<HomeScreen> {
-  List<WebtoonModel> webtoons = [];
-  bool isLoading = true;
-
-  void waitForWebToons() async {
-    webtoons = await ApiService.getTodaysToons();
-    isLoading = false;
-    setState(() {});
-  }
-
-  @override
-  void initState() {
-    // TODO: implement initState
-    super.initState();
-    waitForWebToons();
-  }
+  Future<List<WebtoonModel>> webtoons = ApiService.getTodaysToons();
 
   @override
   Widget build(BuildContext context) {
     print(webtoons);
-    print(isLoading);
     return Scaffold(
-      backgroundColor: Colors.white,
-      appBar: AppBar(
-        elevation: 2,
         backgroundColor: Colors.white,
-        foregroundColor: Colors.green,
-        title: const Text(
-          "오늘의 웹툰",
-          style: TextStyle(
-            fontSize: 24,
+        appBar: AppBar(
+          elevation: 2,
+          backgroundColor: Colors.white,
+          foregroundColor: Colors.green,
+          title: const Text(
+            "오늘의 웹툰",
+            style: TextStyle(
+              fontSize: 24,
+            ),
           ),
         ),
-      ),
-    );
+        body: FutureBuilder(
+          future: webtoons,
+          builder: (context, snapshot) {
+            if (snapshot.hasData) {
+              return const Text("There is data!");
+            }
+            return const Text("Loading....");
+          },
+        ));
   }
 }
