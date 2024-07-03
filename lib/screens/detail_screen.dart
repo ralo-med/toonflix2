a6c83f7553ea238e7e994a8deb891e45794e9595
Ralo <74175604+ralo-med@users.noreply.github.com>
2024-07-03 15:54:17 +0900

    #6.10 Hero

--- lib/screens/detail_screen.dart
+++ lib/screens/detail_screen.dart
@@ -32,25 +32,29 @@ class DetailScreen extends StatelessWidget {
             Row(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                Container(
-                  width: 250,
-                  clipBehavior: Clip.hardEdge,
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(15),
-                    boxShadow: [
-                      BoxShadow(
+                Hero(
+                  tag: id,
+                  child: Container(
+                    width: 250,
+                    clipBehavior: Clip.hardEdge,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(15),
+                      boxShadow: [
+                        BoxShadow(
                           blurRadius: 15,
                           offset: const Offset(10, 10),
-                          color: Colors.black.withOpacity(0.5)),
-                    ],
-                  ),
-                  child: Image.network(
-                    thumb,
-                    headers: const {
-                      "User-Agent":
-                          "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36",
-                      'Referer': 'https://comic.naver.com',
-                    },
+                          color: Colors.black.withOpacity(0.3),
+                        )
+                      ],
+                    ),
+                    child: Image.network(
+                      thumb,
+                      headers: const {
+                        "User-Agent":
+                            "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36",
+                        'Referer': 'https://comic.naver.com',
+                      },
+                    ),
                   ),
                 ),
               ],
